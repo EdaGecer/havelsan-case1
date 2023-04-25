57fb0e39dbd8f6afc128bf055ff014dad387c14d
EdaGecer <eda@eda>
2023-04-25 23:39:55 +0300

    Firs Commit

---
+++ src/firstProject/firstProject.java
@@ -0,0 +1,10 @@
+package firstProject;
+
+public class firstProject {
+
+	public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+        // Hello World!
+        
+    }
+}
